669d98225d7b4b0a0214a2ffcc87b3cc98f79cbd
sergera <sergera88@gmail.com>
2022-04-15 13:36:36 -0300

    Recursive Types
    - Added recursive types for objects, this should help getting rid of "any"
    keywords

--- web-app/src/types.ts
+++ web-app/src/types.ts
@@ -3,6 +3,19 @@ export interface LooseObject {
 	[key: string]: any;
 };
 
+export interface JsonObject {
+	[key: string]: JsonValue;
+};
+
+export type JsonValue =
+		| JsonObject
+    | string
+    | number
+    | boolean
+    | JsonArray;
+
+export interface JsonArray extends Array<JsonValue> {};
+
 export type AbstractClass<T> = Function & {prototype: T};
 
 export type ConstructorClass<T> = new (...args: any[]) => T;
