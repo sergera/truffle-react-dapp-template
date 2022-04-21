e7b75a02cbff38dc94b4e0b1095e7da080873fb0
sergera <sergera88@gmail.com>
2022-04-21 20:02:02 -0300

    Removal of Superfluous Packages
    - Removed "keccak" package and replaced its usage with web3
    
    - Removed "dotenv" package, which had no use

--- web-app/src/format/eth/address/address.ts
+++ web-app/src/format/eth/address/address.ts
@@ -1,17 +1,7 @@
-import createKeccakHash from 'keccak';
+import { utils } from '../../../blockchain/web3';
 
 export const toCheckSum = (ethAdress: string) => {
-  ethAdress = ethAdress.toLowerCase().replace('0x', '');
-  let hash = createKeccakHash('keccak256').update(ethAdress).digest('hex');
-	let formattedAddress = '';
-  for (var i = 0; i < ethAdress.length; i++) {
-    if (parseInt(hash[i], 16) >= 8) {
-      formattedAddress += ethAdress[i].toUpperCase();
-    } else {
-      formattedAddress += ethAdress[i];
-    }
-  }
-  return `0x${formattedAddress}`;
+	return utils.toChecksumAddress(ethAdress);
 };
 
 export const minify = (ethAdress: string) => {
