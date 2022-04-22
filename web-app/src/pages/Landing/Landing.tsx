ccdc7473f0db9fa40abe4d602b440a03acda19d2
sergera <sergera88@gmail.com>
2022-04-22 05:07:06 -0300

    Landing Page Indentation Fix
    - Fixed indentation of landing page component

--- web-app/src/pages/Landing/Landing.tsx
+++ web-app/src/pages/Landing/Landing.tsx
@@ -4,39 +4,39 @@ export function Landing() {
 
   return (
     <div className="landing">
-				<h1>Welcome to the Truffle-React dApp Template</h1>
+			<h1>Welcome to the Truffle-React dApp Template</h1>
 
-				<p>With this template you can jump-start the development of your next dApp!</p>
+			<p>With this template you can jump-start the development of your next dApp!</p>
 
-				<h3>Features:</h3>
-				<ul>
-					<li>Redux store with blockchain-related state management</li>
-					<li>Integration with MetaMask provider from the start</li>
-					<li>Killswitch state for building blockchain-safe components</li>
-					<li>Tested store and components with jest</li>
-					<li>Modals and error notifications for user feedback</li>
-					<li>Completely responsive interface</li>
-					<li>Logging with severity levels</li>
-					<li>Validation and formatting helpers</li>
-					<li>Error page triggered by react boundary</li>
-					<li>Not found page</li>
-				</ul>
+			<h3>Features:</h3>
+			<ul>
+				<li>Redux store with blockchain-related state management</li>
+				<li>Integration with MetaMask provider from the start</li>
+				<li>Killswitch state for building blockchain-safe components</li>
+				<li>Tested store and components with jest</li>
+				<li>Modals and error notifications for user feedback</li>
+				<li>Completely responsive interface</li>
+				<li>Logging with severity levels</li>
+				<li>Validation and formatting helpers</li>
+				<li>Error page triggered by react boundary</li>
+				<li>Not found page</li>
+			</ul>
 
-				<Link 
-					to="/components" 
-					className="landing__link"
-				>
-					Components Page:
-				</Link>
-				<p>A collection of the components built for this template</p>
+			<Link 
+				to="/components" 
+				className="landing__link"
+			>
+				Components Page:
+			</Link>
+			<p>A collection of the components built for this template</p>
 
-				<Link 
-					to="/notes" 
-					className="landing__link"
-				>
-					Notes Contract Page:
-				</Link>
-				<p>A preview of contract interaction with using the "Notes" contract provided as an example</p>
+			<Link 
+				to="/notes" 
+				className="landing__link"
+			>
+				Notes Contract Page:
+			</Link>
+			<p>A preview of contract interaction with using the "Notes" contract provided as an example</p>
     </div>
   );
 };
