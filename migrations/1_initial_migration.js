49db789022fbd7f9e13f74578ff9ccd70fa7bbba
sergera <sergera88@gmail.com>
2022-05-02 17:36:24 -0300

    Separate Deployer Scripts & Unified Versioning
    - Migrations contract and user made contract now have separate scripts
    
    - Version field was removed from the package.json in "web-app" and will be
    controlled only in the root directory package.json
    
    - The project has 2 package.json files, one in the root directory for truffle
    related dependencies (dependencies for contract development and deployment),
    and one in the "web-app" directory for web app specific dependencies, this
    will continue for now for better dependency separation

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,5 @@
 const Migrations = artifacts.require("Migrations");
-const Notes = artifacts.require("Notes");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
-	deployer.deploy(Notes);
 };
