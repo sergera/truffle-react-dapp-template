2a63d7aab278c35278b001e955844b99f56f0493
sergera <sergera88@gmail.com>
2022-05-04 18:06:47 -0300

    Redux Store Type
    - Added redux store type export to store.ts file

--- web-app/src/state/store.ts
+++ web-app/src/state/store.ts
@@ -27,3 +27,4 @@ export const useAppDispatch = () => useDispatch<Dispatch>();
 
 export type Dispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
+export type Store = typeof store;
